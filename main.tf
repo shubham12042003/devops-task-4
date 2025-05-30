f1122faa88c402c4530cd47a19b32437a9dd9f54
Ubuntu <ubuntu@ip-172-31-94-66.ec2.internal>
2025-05-30 13:37:11 +0000

    Completed Task 3 - Terraform Docker

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx_container"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
